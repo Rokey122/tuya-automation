5f61544ba5de2090c7b0650c764a77c9c9254dd8
Karlo Kova <kovakarlo12@gmail.com>
2024-02-17 15:40:17 +0100

    moved headers to header folder

---
+++ header/light.hpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <sstream>
+#include <vector>
+#include "tuyaAPI33.hpp"
+
+#define PORT 6668
+
+class Bulb{
+    public:
+        Bulb(std::string id, std::string key, std::string ip, int switch_led_code);
+
+        void on_off_switch(int state);
+
+    private:
+        tuyaAPI33 tuya;
+        int switch_led_code;
+        std::string id;
+        std::string key;
+        std::string ip;
+
+};
+
+class Lights{
+    public:
+        Lights(const std::vector<Bulb> &bulbs);
+
+        void on_off_switch(int state);
+
+        int get_state();
+        void change_state(int val);
+    
+    private:
+        std::vector <Bulb> bulbs;
+        int state; // 0 is off, 1 is on
+};
--- header/phone.hpp
+++ header/phone.hpp
@@ -1,12 +1,7 @@
 #include <string>
-#include <sstream>
-#include <vector>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
-#include "../tuyapp/src/tuyaAPI33.hpp"
-
-#define PORT 6668
 
 class Phone{
     public:
@@ -28,33 +23,4 @@ class Phone{
         bdaddr_t* bluetooth_mac;
         std::string wifi_mac;
         int dd;
-};
-
-class Bulb{
-    public:
-        Bulb(std::string id, std::string key, std::string ip, int switch_led_code);
-
-        void on_off_switch(int state);
-
-    private:
-        tuyaAPI33 tuya;
-        int switch_led_code;
-        std::string id;
-        std::string key;
-        std::string ip;
-
-};
-
-class Lights{
-    public:
-        Lights(const std::vector<Bulb> &bulbs);
-
-        void on_off_switch(int state);
-
-        int get_state();
-        void change_state(int val);
-    
-    private:
-        std::vector <Bulb> bulbs;
-        int state; // 0 is off, 1 is on
 };
